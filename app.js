a867c6f556f68f718af0d6fbebecc539810dd8ba
lenders-game <98999083+lenders-game@users.noreply.github.com>
2022-06-08 14:35:45 -0700

    adds functionality to change csv to json object

--- app.js
+++ app.js
@@ -1,14 +1,38 @@
 const express = require('express');
 const app = express();
-// const data = 
-// {
-//   "data": []
-// }
+const fs = require('fs');
+
 app.use(express.json())
 
 app.get('/', (req, res) => {
-  res.status(200).send(`Server is running. Please navigate to the proper url endpoint.`)
+  res.status(200).send(`hello world`)
   console.log(req.body);
+})
+
+app.get('/books', (req, res) => {  
+  let result = []
+  let bookData = fs.readFileSync('./dummy.csv', 'utf8')
+  bookData = bookData.split("\n")
+  bookData.shift()  // shift removes first element of array because we don't need headers
+
+  for (let i in bookData) {   // you want the index not the element
+    bookData[i] = bookData[i].split(`,`)
+    let temp = {
+      id: parseInt(bookData[i][0].replaceAll(`\"`, ``)),
+      title: bookData[i][1].replaceAll(`\"`, ``),
+      author: bookData[i][2].replaceAll(`\"`, ``),
+      cover: bookData[i][3].replaceAll(`\"`, ``)
+    }
+    result.push(temp)
+  }
+
+  res.status(200).send(result)
+})
+
+
+app.post('/books', (req, res) => {
+ 
+  
 })
 
 module.exports = app;
--- index.test.js
+++ index.test.js
@@ -1,4 +1,15 @@
-const request = require('supertest');
+const req = require('supertest');
 const app = require('./app')
 
 
+describe('/books endpoint', () => {
+  test('GET /books should output a hello world message', (done)  =>{
+    req(app)
+      .get('/books')
+      .expect(200)
+      .end((err, res) => {
+        if (err) throw err;
+        done()
+      })
+  })
+})
